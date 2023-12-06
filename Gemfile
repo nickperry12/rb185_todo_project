68cff907bb138016101dd30012d1e916f231ccd8
Nick Perry <nick.perry12@outlook.com>
2023-12-06 13:37:05 -0800

    moved SessionPersistence class to own file and created a DataPersistence file and class

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem "sinatra"
 gem "sinatra-contrib"
 gem "erubis"
 gem "webrick"
+gem "pg"
 
 ruby "3.2.2"
 
--- todo.rb
+++ todo.rb
@@ -3,6 +3,8 @@ require "sinatra/reloader" if development?
 require "sinatra/content_for"
 require "tilt/erubis"
 
+require_relative "database_persistence"
+
 configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(32)
@@ -42,80 +44,6 @@ helpers do
   end
 end
 
-class SessionPersistence
-  def initialize(session)
-    @session = session
-    @session[:lists] ||= []
-  end
-  
-  def find_list(id)
-    session[:lists].find{ |list| list[:id] == id }
-  end
-
-  def all_lists
-    session[:lists]
-  end
-
-  def success_list_removal
-    session[:success] = "The list has been successfully deleted."
-  end
-
-  def set_session_success_message(message)
-    session[:success] = message
-  end
-
-  def set_session_error_message(error)
-    session[:error] = error
-  end
-
-  def delete_todo_list(id)
-    session[:lists].reject! { |list| list[:id] == id }
-  end
-
-  def create_new_todo_list(list_name)
-    id = next_element_id(@session[:lists])
-    session[:lists] << { id: id, name: list_name, todos: [] }
-  end
-
-  def update_list_name(id, new_name)
-    list = find_list(id)
-    list[:name] = new_name
-  end
-
-  def create_new_todo(list_id, todo_name)
-    list = find_list(list_id)
-    id = next_element_id(list[:todos])
-    list[:todos] << { id: id, name: todo_name, completed: false }
-  end
-
-  def delete_todo_from_list(list_id, todo_id)
-    list = find_list(list_id)
-    list[:todos].reject! { |todo| todo[:id] == todo_id }
-  end
-
-  def update_todo_status(list_id, todo_id, new_status)
-    list = find_list(list_id)
-    todo = list[:todos].find { |todo| todo[:id] == todo_id }
-    todo[:completed] = new_status
-  end
-
-  def mark_all_todos_complete(list_id)
-    list = find_list(list_id)
-    list[:todos].each do |todo|
-      todo[:completed] = true
-    end
-  end
-
-  private
-
-  def next_element_id(elements)
-    max = elements.map { |todo| todo[:id] }.max || 0
-    max + 1
-  end
-
-  attr_reader :session
-end
-
 def load_list(id)
   list = @storage.find_list(id)
   return list if list
@@ -142,7 +70,7 @@ def error_for_todo(name)
 end
 
 before do
-  @storage = SessionPersistence.new(session)
+  @storage = DatabasePersistence.new
 end
 
 get "/" do
